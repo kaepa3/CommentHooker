965d780011af8b228b6c56c9e8c7ec7c0a37459e
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2022-08-03 00:05:02 +0900

    Changes to be committed: 	modified:   cmd/message.sh

--- cmd/message.sh
+++ cmd/message.sh
@@ -4,7 +4,10 @@ readonly COMMIT_MSG_FILE=$1
 readonly COMMIT_SOURCE=$2
 
 # ブランチ名取得
-ranch_name=$(git name-rev --name-only HEAD)
-
+branch_name=$(git name-rev --name-only HEAD)
+nsg="s/\[branch-name\]/$current_branch/"
+echo $1 
+echo $msg
 # コミットメッセージファイル書込み
-sed -i "s/\[branch-name\]/$current_branch/" $1
+sed -i "" "s/\[branch-name\]/$current_branch/" $1
+
