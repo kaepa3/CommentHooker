5740d515d8bebbb6a4eda4af06364b055e679a74
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2022-08-03 01:12:23 +0900

    change shell

--- cmd/message.sh
+++ cmd/message.sh
@@ -1,13 +1,32 @@
-#!/bin/bash
+#!/bin/sh
 
-readonly COMMIT_MSG_FILE=$1
-readonly COMMIT_SOURCE=$2
+branchPath=$(git symbolic-ref -q HEAD) # branchPath は refs/heads/feature/XXXX_YYYY のような文字列に
+branchName=${branchPath##*/} # 最後の / 以下を取得し、branchName は XXXX_YYYY のような文字列に
+issueNumber=$(echo $branchName | cut -d "_" -f 1) # "_" を delimiter として cut し、issueNumber は XXXX に
+firstLine=$(head -n1 $1)
 
-# ブランチ名取得
-branch_name=$(git name-rev --name-only HEAD)
-nsg="s/\[branch-name\]/$current_branch/"
-echo $1 
-echo $msg
-# コミットメッセージファイル書込み
-sed -i "" "s/\[branch-name\]/$current_branch/" $1
+sed -i "1s/^/($issueNumber) \n/" $1 # コミットメッセージの先頭に (XXXX) という文字列でブランチの情報を追加。
+
+# This hook includes three examples.  The first comments out the
+# "Conflicts:" part of a merge commit.
+#
+# The second includes the output of "git diff --name-status -r"
+# into the message, just before the "git status" output.  It is
+# commented because it doesn't cope with --amend or with squashed
+# commits.
+#
+# The third example adds a Signed-off-by line to the message, that can
+# still be edited.  This is rarely a good idea.
+
+case "$2,$3" in
+  merge,)
+    /usr/bin/perl -i.bak -ne 's/^/# /, s/^# #/#/ if /^Conflicts/ .. /#/; print' "$1" ;;
+
+# ,|template,)
+#   /usr/bin/perl -i.bak -pe '
+#      print "\n" . `git diff --cached --name-status -r`
+#	 if /^#/ && $first++ == 0' "$1" ;;
+
+  *) ;;
+esac
 
