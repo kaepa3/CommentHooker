698fd828a2a4532e0f441d55820814320e71d82f
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2022-08-06 03:08:35 +0900

    fix description

--- cmd/version.go
+++ cmd/version.go
@@ -17,13 +17,8 @@ var version string
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "dispay now chooker version",
+	Long:  `dispay now chooker version`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("version:%s", version)
 	},
