603f1d945c9850779f48a6c0a4932f865f2c78d9
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2022-08-07 18:28:24 +0900

    change ver fil read way

--- verup.sh
+++ verup.sh
@@ -6,7 +6,9 @@ git merge develop
 verPath="./cmd/resource/version.txt"
 
 # ver increment
-awk '{print $1}' $verPath  | ( IFS=".$IFS" ; read a b c && echo $a.$b.$((c + 1)) >"$verPath" ) 
+Ver=$(<"$verPath")
+echo $Ver | ( IFS=".$IFS" ; read a b c && echo $a.$b.$((c + 1)) >"$verPath" ) 
+
 chmod 777 $verPath
 Ver=$(<"$verPath")
 echo $Ver
